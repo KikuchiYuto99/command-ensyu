6b37d553b97a04503ab3d13b834ea95c7dc72f76
yutokikuchi <yuuto.kikuchi.sys23@morijyobi.ac.jp>
2023-11-09 15:09:40 +0900

    2人分のBMIの計測

--- Main.java
+++ Main.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("身長と体重を入力してください");
+        System.out.println("2人分の身長と体重を入力してください");
         double height = sc.nextDouble();
         double weight = sc.nextDouble();
+        double height2 = sc.nextDouble();
+        double weight2 = sc.nextDouble();
         double height_bmi = height / 100;
+        double height_bmi2 = height2 / 100;
         double bmi = weight / (height_bmi * height_bmi);
+        double bmi3 = weight2 / (height_bmi2 * height_bmi2);
         String sv = String.format("%.2f", bmi);
+        String sv2 = String.format("%.2f", bmi3);
         double bmi2 = Double.parseDouble(sv);
-        System.out.println("あなたのBMIは" + bmi2 + "です");
+        double bmi4 = Double.parseDouble(sv2);
+        System.out.println("1人目のBMIは" + bmi2 + "です");
+        System.out.println("2人目のBMIは" + bmi4 + "です");
     }
 }
+

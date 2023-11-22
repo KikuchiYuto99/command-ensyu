e2f217d8166e22e93c8a5be7536d787c80a15476
yutokikuchi <yuuto.kikuchi.sys23@morijyobi.ac.jp>
2023-11-22 11:47:53 +0900

    Bmi.javaの修正

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("2人分の身長と体重を入力してください");
@@ -20,4 +20,5 @@ public class Main {
         System.out.println("2人目のBMIは" + bmi4 + "です");
     }
 }
+
 
